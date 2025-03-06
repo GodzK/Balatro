edbe24e1b720e7d7b952d7477e5eb2e1ebebe09a
Phakaphol Dherachaisuphakij <godzk25@gmail.com>
2025-03-06 15:02:31 +0700

    commit for streak🔥

---
+++ Frontendmentor/Frontendmentor/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='container'>
+      <div className='card'>
+        <h2>Stay updated!</h2>
+      </div>
+    </div>
+  )
+}
+
+export default App
